cbdfd22967741d489a874d340c617ff44c435151
Douglas Rezabek <dougabugg@users.noreply.github.com>
2017-08-01 17:19:05 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+# copied from https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'rdparse',
+    version='0.0.1',
+    description='A quick and dirty recursive descent parser written in Python 3',
+    long_description=long_description,
+    url="https://github.com/dougabugg/python-recursive-descent-parser",
+    author="Douglas Rezabek",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords="recursive descent parsing frontend",
+    python_requires='>=3',
+)
